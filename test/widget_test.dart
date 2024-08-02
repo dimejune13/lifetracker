c7cc330f35c8fedd82a0b25530a8549795c82218
dimejune13 <11712613+dimejune13@users.noreply.github.com>
2024-08-02 20:28:11 +0700

    Create GitHub Action for testing Flutter

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,33 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:lifetracker/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Test Expense Addition', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Expense: 0'), findsOne);
+    expect(find.text('Expense: 1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Expense: 0'), findsNothing);
+    expect(find.text('Expense: 1'), findsOne);
+  });
+
+  testWidgets('Test Income Addition', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.text('Income: 0'), findsOne);
+    expect(find.text('Income: 1'), findsNothing);
+
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    expect(find.text('Income: 1'), findsOne);
+    expect(find.text('Income: 0'), findsNothing);
   });
 }
