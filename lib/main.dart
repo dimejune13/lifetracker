004adf515159282e584c864141125f290e705432
dimejune13 <11712613+dimejune13@users.noreply.github.com>
2024-08-03 10:35:50 +0700

    Added boilerplate code for floating button speed dial

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,6 +33,31 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _income = 0;
   int _expense = 0;
+
+  var renderOverlay = true;
+  var visible = true;
+  var switchLabelPosition = false;
+  var extend = false;
+  var mini = false;
+  var rmicons = false;
+  var customDialRoot = false;
+  var closeManually = false;
+  var useRAnimation = true;
+  var isDialOpen = ValueNotifier<bool>(false);
+  var speedDialDirection = SpeedDialDirection.up;
+  var buttonSize = const Size(56.0, 56.0);
+  var childrenButtonSize = const Size(56.0, 56.0);
+  var selectedfABLocation = FloatingActionButtonLocation.endDocked;
+  var items = [
+    FloatingActionButtonLocation.startFloat,
+    FloatingActionButtonLocation.startDocked,
+    FloatingActionButtonLocation.centerFloat,
+    FloatingActionButtonLocation.endFloat,
+    FloatingActionButtonLocation.endDocked,
+    FloatingActionButtonLocation.startTop,
+    FloatingActionButtonLocation.centerTop,
+    FloatingActionButtonLocation.endTop,
+  ];
 
   void _addIncome() {
     setState(() {
@@ -80,11 +106,100 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addExpense,
-        tooltip: 'Add Expense',
-        child: const Icon(Icons.add),
-      ),
+      floatingActionButton: SpeedDial(
+          // animatedIcon: AnimatedIcons.menu_close,
+          // animatedIconTheme: IconThemeData(size: 22.0),
+          // child: Text("open"),
+          // activeChild: Text("close"),
+          icon: Icons.add,
+          activeIcon: Icons.close,
+          spacing: 3,
+          mini: mini,
+          openCloseDial: isDialOpen,
+          childPadding: const EdgeInsets.all(5),
+          spaceBetweenChildren: 4,
+          dialRoot: customDialRoot
+              ? (ctx, open, toggleChildren) {
+                  return ElevatedButton(
+                    onPressed: toggleChildren,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue[900],
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 22, vertical: 18),
+                    ),
+                    child: const Text(
+                      "Custom Dial Root",
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  );
+                }
+              : null,
+          buttonSize:
+              buttonSize,
+          label: extend
+              ? const Text("Open")
+              : null,
+          activeLabel: extend ? const Text("Close") : null,
+
+          childrenButtonSize: childrenButtonSize,
+          visible: visible,
+          direction: speedDialDirection,
+          switchLabelPosition: switchLabelPosition,
+
+          closeManually: closeManually,
+
+          renderOverlay: renderOverlay,
+          // overlayColor: Colors.black,
+          // overlayOpacity: 0.5,
+          onOpen: () => debugPrint('OPENING DIAL'),
+          onClose: () => debugPrint('DIAL CLOSED'),
+          useRotationAnimation: useRAnimation,
+          tooltip: 'Open Speed Dial',
+          heroTag: 'speed-dial-hero-tag',
+          // foregroundColor: Colors.black,
+          // backgroundColor: Colors.white,
+          // activeForegroundColor: Colors.red,
+          // activeBackgroundColor: Colors.blue,
+          elevation: 8.0,
+          animationCurve: Curves.elasticInOut,
+          isOpenOnStart: false,
+          shape: customDialRoot
+              ? const RoundedRectangleBorder()
+              : const StadiumBorder(),
+          // childMargin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          children: [
+            SpeedDialChild(
+              child: const Icon(Icons.accessibility),
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+              label: 'Finance',
+              onTap: () => ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text(("Finance Tap")),)
+              ),
+              onLongPress: () => ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text(("Finance Long Pressed")),)
+              ),
+            ),
+            SpeedDialChild(
+              child: !rmicons ? const Icon(Icons.brush) : null,
+              backgroundColor: Colors.deepOrange,
+              foregroundColor: Colors.white,
+              label: 'Second',
+              visible: false,
+              onTap: () => debugPrint('SECOND CHILD'),
+            ),
+            SpeedDialChild(
+              child: !rmicons ? const Icon(Icons.margin) : null,
+              backgroundColor: Colors.indigo,
+              foregroundColor: Colors.white,
+              label: 'Show Snackbar',
+              visible: false,
+              onTap: () => ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text(("Third Child Pressed")))),
+              onLongPress: () => debugPrint('THIRD CHILD LONG PRESS'),
+            ),
+          ],
+        ),
     );
   }
 }
