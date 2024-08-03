5152d4335520aa2e0c38d4cbbabace6a7f2b32fe
dimejune13 <11712613+dimejune13@users.noreply.github.com>
2024-08-03 10:21:14 +0700

    Delete unit tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,32 +12,10 @@ import 'package:lifetracker/main.dart';
 
 void main() {
   testWidgets('Test Expense Addition', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('Expense: 0'), findsOne);
-    expect(find.text('Expense: 1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('Expense: 0'), findsNothing);
-    expect(find.text('Expense: 1'), findsOne);
   });
 
   testWidgets('Test Income Addition', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
 
-    expect(find.text('Income: 0'), findsOne);
-    expect(find.text('Income: 1'), findsNothing);
-
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    expect(find.text('Income: 1'), findsOne);
-    expect(find.text('Income: 0'), findsNothing);
   });
 }
