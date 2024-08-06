d5c900ba677bb7452f79a4cfbd20f6c6481aa089
dimejune13 <11712613+dimejune13@users.noreply.github.com>
2024-08-06 22:22:47 +0700

    Deleted most of HomePage code to be refactored

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
-
-import 'package:lifetracker/src/features/financial/presentation/route.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,32 +31,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  var renderOverlay = true;
-  var visible = true;
-  var switchLabelPosition = false;
-  var extend = false;
-  var mini = false;
-  var rmicons = false;
-  var customDialRoot = false;
-  var closeManually = false;
-  var useRAnimation = true;
-  var isDialOpen = ValueNotifier<bool>(false);
-  var speedDialDirection = SpeedDialDirection.up;
-  var buttonSize = const Size(56.0, 56.0);
-  var childrenButtonSize = const Size(56.0, 56.0);
-  var selectedfABLocation = FloatingActionButtonLocation.endDocked;
-  var items = [
-    FloatingActionButtonLocation.startFloat,
-    FloatingActionButtonLocation.startDocked,
-    FloatingActionButtonLocation.centerFloat,
-    FloatingActionButtonLocation.endFloat,
-    FloatingActionButtonLocation.endDocked,
-    FloatingActionButtonLocation.startTop,
-    FloatingActionButtonLocation.centerTop,
-    FloatingActionButtonLocation.endTop,
-  ];
-
-
   @override
   Widget build(BuildContext context) {
 
@@ -69,121 +40,8 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: SafeArea(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            children: <Widget>[
-              const Text(
-                'Welcome to Life Tracker App',
-              ),
-              const Text(
-                'Choose what to track:'
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => FinancialRoute())
-                  );
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: const Text(
-                    'Financial'
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () => ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text(("Health Tracker Coming Soon")),)
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: const Text(
-                    'Health'
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+        child: Text("LifeTracker"),
       ),
-      floatingActionButton: SpeedDial(
-          // animatedIcon: AnimatedIcons.menu_close,
-          // animatedIconTheme: IconThemeData(size: 22.0),
-          // child: Text("open"),
-          // activeChild: Text("close"),
-          icon: Icons.add,
-          activeIcon: Icons.close,
-          spacing: 3,
-          mini: mini,
-          openCloseDial: isDialOpen,
-          childPadding: const EdgeInsets.all(5),
-          spaceBetweenChildren: 4,
-          dialRoot: customDialRoot
-              ? (ctx, open, toggleChildren) {
-                  return ElevatedButton(
-                    onPressed: toggleChildren,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue[900],
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 22, vertical: 18),
-                    ),
-                    child: const Text(
-                      "Custom Dial Root",
-                      style: TextStyle(fontSize: 17),
-                    ),
-                  );
-                }
-              : null,
-          buttonSize:
-              buttonSize,
-          label: extend
-              ? const Text("Open")
-              : null,
-          activeLabel: extend ? const Text("Close") : null,
-
-          childrenButtonSize: childrenButtonSize,
-          visible: visible,
-          direction: speedDialDirection,
-          switchLabelPosition: switchLabelPosition,
-
-          closeManually: closeManually,
-
-          renderOverlay: renderOverlay,
-          // overlayColor: Colors.black,
-          // overlayOpacity: 0.5,
-          onOpen: () => debugPrint('OPENING DIAL'),
-          onClose: () => debugPrint('DIAL CLOSED'),
-          useRotationAnimation: useRAnimation,
-          tooltip: 'Open Speed Dial',
-          heroTag: 'speed-dial-hero-tag',
-          // foregroundColor: Colors.black,
-          // backgroundColor: Colors.white,
-          // activeForegroundColor: Colors.red,
-          // activeBackgroundColor: Colors.blue,
-          elevation: 8.0,
-          animationCurve: Curves.elasticInOut,
-          isOpenOnStart: false,
-          shape: customDialRoot
-              ? const RoundedRectangleBorder()
-              : const StadiumBorder(),
-          // childMargin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          children: [
-            SpeedDialChild(
-              child: const Icon(Icons.attach_money),
-              backgroundColor: Colors.red,
-              foregroundColor: Colors.white,
-              label: 'Finance',
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => FinancialRoute())
-                );
-              },
-            ),
-          ],
-        ),
     );
   }
 }
