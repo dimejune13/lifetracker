02262d9ff710f26cb1d86417eef7cc549235aabf
dimejune13 <11712613+dimejune13@users.noreply.github.com>
2024-08-18 16:22:41 +0700

    Added Financial Form with dropdown button for payment transaction type

--- lib/src/presentation/financial_page.dart
+++ lib/src/presentation/financial_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:lifetracker/constants.dart';
+
 class FinancialPage extends StatelessWidget {
   const FinancialPage({Key? key}) : super(key: key);
 
@@ -17,9 +19,66 @@ class FinancialPage extends StatelessWidget {
           color: Theme.of(context).colorScheme.primaryContainer,
         ),
       ),
-      body: Column(
-        children: [
-          Text("Financial Form"),
+      body: Container(
+        margin: EdgeInsets.only(
+          left: kDefaultPadding,
+          right: kDefaultPadding,
+          top: kDefaultPadding,
+          bottom: kDefaultPadding * 2.5,
+        ),
+        child: FinancialForm(),
+      ),
+    );
+  }
+}
+
+class FinancialForm extends StatefulWidget {
+  const FinancialForm({Key? key}) : super(key: key);
+
+  @override
+  _FinancialFormState createState() => _FinancialFormState();
+}
+
+class _FinancialFormState extends State<FinancialForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? selectedValue = 'qris';
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Payment Transaction',
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          DropdownButtonFormField<String>(
+            value: selectedValue,
+            hint: Text('Select a payment transaction'),
+            onChanged: (newValue) {
+              setState(() {
+                selectedValue = newValue;
+              });
+            },
+            validator: (value) {
+              if (value == null) {
+                return 'Please select a payment transaction';
+              }
+              return null;
+            },
+            items: <String>['qris', 'cash', 'debit_card', 'credit_card', 'ewallet', 'bank_transfer']
+              .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+          ),
         ],
       ),
     );
